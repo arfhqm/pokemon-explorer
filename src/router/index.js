ad0083157fa36ebae27f785ea528bb3619cb05fc
arfhqm <arifhaqeem.noh@gmail.com>
2025-08-07 01:23:57 +0800

    Corrected router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Home from '../pages/Home.vue'
 import PokemonDetail from '../pages/PokemonDetail.vue'
-import axios from 'axios'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/pokemon/:name',
+    name: 'PokemonDetail',
+    component: PokemonDetail
+  }
+]
 
 const router = createRouter({
   history: createWebHistory('/pokemon-explorer/'),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: () => import('../views/HomeView.vue')
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-  ],
+  routes
 })
 
 export default router
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/pokemon/:name', component: PokemonDetail }
-]
