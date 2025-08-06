78f5dd4ba7578cc5f87709ed7de6e3a849778cab
arfhqm <arifhaqeem.noh@gmail.com>
2025-08-07 01:13:18 +0800

    Update Vue Router History Base

--- src/router/index.js
+++ src/router/index.js
@@ -6,32 +6,24 @@ import axios from 'axios'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/pokemon-explorer/'),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Home',
+      component: () => import('../views/HomeView.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue'),
+    // },
   ],
 })
 
-// export default router
+export default router
 
 const routes = [
   { path: '/', component: Home },
   { path: '/pokemon/:name', component: PokemonDetail }
 ]
-
-export default createRouter({
-  history: createWebHistory(),
-  routes
-})
